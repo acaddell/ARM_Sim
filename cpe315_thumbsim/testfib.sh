46a49d2bff157a5edf94bd0a9187e4c6bf525cb5
cclark18 <cclark18@calpoly.edu>
2014-06-02 14:33:44 -0700

    updated input and output test files

--- cpe315_thumbsim/testfib.sh
+++ cpe315_thumbsim/testfib.sh
@@ -1,4 +1,8 @@
 make
-echo ----- test -----
+echo "----- test (old) -----"
 make run > my_fib.out
 diff fib.out my_fib.out
+
+echo "----- test (new) -----"
+./thumbsim -s -c 256 -i -d -f inputs/fib.sim > my_fib.out
+diff fib.completeoutput my_fib.out
